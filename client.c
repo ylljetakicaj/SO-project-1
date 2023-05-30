f2babcc5b9104f3146699a6ddadc045c81a50385
Ylljete Kicaj <118022552+ylljetakicaj@users.noreply.github.com>
2023-05-30 15:05:53 +0200

    Update client.c

--- client.c
+++ client.c
@@ -1,1 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <unistd.h>
 
+
+#define MSG_SIZE 256
+#define MAX_CLIENTS 5
+
+typedef struct {
+    int client_connected;
+    char msg_text[MSG_SIZE];
+} SharedData;
+
+int main() {
+    key_t key;
+    int shm_id;
+    SharedData* shared_data;
+
+    if ((key = ftok("server8.c", 'B')) == -1) {
+        perror("ftok");
+        exit(1);
+    }
+
+    if ((shm_id = shmget(key, sizeof(SharedData) * MAX_CLIENTS, 0644)) == -1) {
+        perror("shmget");
+        exit(1);
+    }
+
+    if ((shared_data = (SharedData*)shmat(shm_id, NULL, 0)) == (void*)-1) {
+        perror("shmat");
+        exit(1);
+    }
+
+    printf("Connected to the server.\n");
